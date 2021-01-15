fe1f25249d5d8ae0e428a04f0910de26db3872c2
Abdul <api@proofratings.com>
2021-01-15 14:59:40 -0800

    updated npm packages

--- pipelines_webinar/pipelines_webinar_stack.ts
+++ pipelines_webinar/pipelines_webinar_stack.ts
@@ -37,7 +37,7 @@ export class PipelinesWebinarStack extends Stack {
         code: synthetics.Code.fromAsset(path.join(__dirname, 'canary')),
         handler: 'apiCall.handler',
       }),
-      runtime: synthetics.Runtime.SYNTHETICS_NODEJS_2_0,
+      runtime: synthetics.Runtime.SYNTHETICS_NODEJS_2_2,
       startAfterCreation: false
     });
 
