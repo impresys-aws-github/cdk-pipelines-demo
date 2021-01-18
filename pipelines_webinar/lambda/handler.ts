0a017ccd7da4c594b42624dd3a1aa1bcca022a5d
aws-github <60489891+impresys-aws-github@users.noreply.github.com>
2021-01-18 05:29:29 -0800

    changed msg

--- pipelines_webinar/lambda/handler.ts
+++ pipelines_webinar/lambda/handler.ts
@@ -2,7 +2,7 @@ const AWS = require('aws-sdk');
 
 export const handler = async (event: any = {}): Promise<any> => {
     return {
-        body: 'OK',
+        body: 'OKAY',
         statusCode: 200,
     };
 };
