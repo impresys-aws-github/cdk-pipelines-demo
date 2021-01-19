6b750bc132eba5a858c6745581fe4fe3e9986fc3
impresys-aws-github <60489891+impresys-aws-github@users.noreply.github.com>
2021-01-19 01:19:15 -0800

    Update handler.ts

--- pipelines_webinar/lambda/handler.ts
+++ pipelines_webinar/lambda/handler.ts
@@ -2,7 +2,7 @@ const AWS = require('aws-sdk');
 
 export const handler = async (event: any = {}): Promise<any> => {
     return {
-        body: 'Break',
-        statusCode: 500,
+        body: 'OK',
+        statusCode: 200,
     };
 };
